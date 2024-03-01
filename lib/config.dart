b86297d4f58b3d1378778fbdf8aea17e0c988cfb
Graffitiii <saksornwattanasit@gmail.com>
2024-03-01 16:30:44 +0700

    cxsads

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
-// final url = 'http://192.168.87.229:3000/';
-final url = 'http://192.168.1.109:3000/';
+final url = 'http://127.0.0.1:3000/';
+// final url = 'http://192.168.1.109:3000/';
 final registration = url + "registration";
 final loginUrl = url + "login";
 final addtodo = url + "storeTodo";
